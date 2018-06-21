23f3f7f7b6153b587ec0fd43b5489ab12fab35fd
ssnaff <40366324+ssnaff@users.noreply.github.com>
2018-06-21 21:12:03 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+const Client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+clinet.on("message", message => {
+  if (message.content === "ping" ) {
+    message.reply("pong");
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
